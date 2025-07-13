619b378823071e2281b21300afcb072288b1a128
Mosammat Israt Arefin Mazumder <nisratafrin826@gmail.com>
2025-07-13 13:37:58 +0600

    Add files via upload

---
+++ Module_02_Assignment_01/Problem 1.py
@@ -0,0 +1,9 @@
+# Write a function that takes two numbers as input and returns their average. Call the function with different values.
+
+def average_of_two_numbers(num1,num2):
+    return(num1+num2)/ 2
+
+print(average_of_two_numbers(10,20))
+print(average_of_two_numbers(5,15))
+print(average_of_two_numbers(2,3))
+print(average_of_two_numbers(0,100))
